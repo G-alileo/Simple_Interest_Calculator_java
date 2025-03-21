1ee17941b5c10786c33d2b72888c92a7d69e49a4
G_alileo <jamespmurithi@gmail.com>
2025-03-22 01:07:42 +0300

    Modified program logic, CLI user friendly, blocked the code into functions for easy readability, Exception handling

--- Main.java
+++ Main.java
@@ -5,60 +5,54 @@ public class Main {
 
     public static void main(String[] args) {
         
-        // initializing variables
-        double principle, rate, time, simpleinterest;
-        principle = rate =  time = 0;
+        // declaring variables
+        boolean validity = true;
+        int user_choice = 2;
 
-        // creating intances of the classes am to use
-        Scanner user_val = new Scanner(System.in);
-        Main obj = new Main();
+        // creating an obj
+        Main my_obj = new Main();
+        Scanner Sc = new Scanner(System.in);
 
-        //Taking user input
+        // initializing infinite loop
+        while (validity) {
+        // System.out.println("Welcome to the simple interest calculator ");
+        System.out.print("\n===========================================================================================\n");
+        System.out.println("\t\tMenu");
+        System.out.println("\n\t1. Start the calculator. ");
+        System.out.println("\t2. Exit ");
+        System.out.print("\nYour choice : ");
+
+        // Validating user input
         try {
-            System.out.println("Welcome to the Simple Interest Calculator.");
-            System.out.println("Enter the Principle : ");
-            principle = user_val.nextDouble();
+            user_choice = Sc.nextInt();
+
+            switch (user_choice) {
+                case 1:
+                    my_obj.LogicMethod();
+                    break;
+    
+                case 2:
+                    validity = false;
+                    Sc.close();
+                    System.out.println("Exiting ..... Goodbye!");
+                    System.out.print("\n===========================================================================================\n");
+                    break;
+            
+                default:
+                    System.out.println("Error : Invalid choice! Out of Bound");
+                    break;
+            }
+            
         } catch (InputMismatchException e) {
-            // TODO: handle exception
-            System.out.println("Error : Invalid input type!");
-            user_val.nextLine();  // Clearing the scanner class
+            System.out.println("Error : Invalid input ! Type Error");
+            Sc.nextLine();
+            // Sc.close();
         }
 
-
-        try {
-            System.out.println("Welcome to the Simple Interest Calculator.");
-            System.out.println("Enter your rate : ");
-            rate = user_val.nextDouble();
-        } catch (InputMismatchException e) {
-            // TODO: handle exception
-            System.out.println("Error : Invalid input type!");
-            user_val.nextLine();
-
         }
-
-
-        try {
-            System.out.println("Welcome to the Simple Interest Calculator.");
-            System.out.println("Enter your time in years : ");
-            time = user_val.nextDouble();
-        } catch (InputMismatchException e) {
-            // TODO: handle exception
-            System.out.println("Error : Invalid input type!");
-            user_val.nextLine(); 
-
-        }
-
-        //calculating the simple interest
-        simpleinterest =  obj.SimpleInterest(principle, rate, time);
-
-        // display the results
-        System.out.println("Results ");
-        System.out.println("Principle = " + principle);
-        System.out.println("Rate = " + rate + " /100");
-        System.out.println("Time = " + time + "yr(s)");
-        System.out.println("Simple Interest = " + simpleinterest );
-
+        
     }
+
 
     // A method to calculate simple interest
     public double SimpleInterest (double principle, double rate, double time) {
@@ -66,5 +60,70 @@ public class Main {
     }
 
     // A method to validate user input
+    public void LogicMethod(){
+         // initializing variables
+         double principle, rate, time, simpleinterest;
+         principle = rate =  time = 0;
+         boolean validity = true;
+ 
+         // creating intances of the classes am to use
+         Scanner user_val = new Scanner(System.in);
+         Main obj = new Main();
+        
+
+         //Starting a loop
+         while (validity) {
+            //Taking user input
+         try {
+            System.out.print("===========================================================================================");
+            System.out.println("\n\t\tWelcome to the Simple Interest Calculator.");
+            System.out.print("Enter the Principle : ");
+            principle = user_val.nextDouble();
+        } catch (InputMismatchException e) {
+            System.out.println("Error : Invalid input type!");
+            user_val.nextLine();  // Clearing the scanner class
+            break;
+        }
+
+
+        try {
+            System.out.println();
+            System.out.print("Enter your rate : ");
+            rate = user_val.nextDouble();
+        } catch (InputMismatchException e) {
+            System.out.println("Error : Invalid input type!");
+            user_val.nextLine();
+            break;
+
+        }
+
+
+        try {
+            System.out.println();
+            System.out.print("Enter your time in years : ");
+            time = user_val.nextDouble();
+            System.out.print("\n===========================================================================================");
+
+        } catch (InputMismatchException e) {
+            System.out.println("Error : Invalid input type!");
+            user_val.nextLine(); 
+            break;
+
+        }
+
+        //calculating the simple interest
+        simpleinterest =  obj.SimpleInterest(principle, rate, time);
+ 
+        // display the results
+        System.out.println("\n\t\tResults ");
+        System.out.println("\n\tPrinciple = " +"KSH " + principle );
+        System.out.println("\tRate = " + rate + " %");
+        System.out.println("\tTime = " + time + " yr(s)");
+        System.out.println("\tSimple Interest = " + "KSH "  + simpleinterest );
+        System.out.print("\n===========================================================================================\n");
+        validity = false;
+        }
+        // user_val.close();
+    }
     
 }
